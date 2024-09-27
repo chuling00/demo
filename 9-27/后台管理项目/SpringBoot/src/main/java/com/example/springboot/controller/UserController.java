aa23451713a1565eb8b658dfc1939fd838aca791
chuling <2755080373@qq.com>
2024-09-27 10:19:23 +0800

    9/27

---
+++ 9-27/后台管理项目/SpringBoot/src/main/java/com/example/springboot/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.example.springboot.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
+import javax.annotation.Resource;
+import java.util.List;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import com.example.springboot.service.IUserService;
+import com.example.springboot.entity.User;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author chuling
+ * @since 2024-09-27
+ */
+@RestController
+@RequestMapping("/user")
+                public class UserController {
+    
+        @Resource
+        private IUserService userService;
+
+        // 新增或者更新
+        @PostMapping
+        public boolean save(@RequestBody User user) {
+            return userService.saveOrUpdate(user);
+        }
+
+        @DeleteMapping("/{id}")
+        public Boolean delete(@PathVariable Integer id) {
+            return userService.removeById(id);
+        }
+
+        @PostMapping("/del/batch")
+        public boolean deleteBatch(@RequestBody List<Integer> ids) {
+            return userService.removeByIds(ids);
+        }
+
+        @GetMapping
+        public List<User> findAll() {
+            return userService.list();
+        }
+
+        @GetMapping("/{id}")
+        public User findOne(@PathVariable Integer id) {
+            return userService.getById(id);
+        }
+
+        @GetMapping("/page")
+        public Page<User> findPage(@RequestParam Integer pageNum,
+                                        @RequestParam Integer pageSize,
+                                        @RequestParam(defaultValue = "") String username,
+                                        @RequestParam(defaultValue = "") String email,
+                                        @RequestParam(defaultValue = "") String address) {
+            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+            queryWrapper.orderByDesc("id");
+            if (!"".equals(username)) {
+                queryWrapper.like("username", username);
+            }
+            if (!"".equals(email)) {
+                queryWrapper.like("email", email);
+            }
+            if (!"".equals(address)) {
+                queryWrapper.like("address", address);
+            }
+            queryWrapper.orderByDesc("id");
+            return userService.page(new Page<>(pageNum, pageSize), queryWrapper);
+        }
+
+}
+
---
+++ 后台管理项目/SpringBoot/src/main/java/com/example/springboot/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.example.springboot.service;
+
+import com.example.springboot.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author chuling
+ * @since 2024-09-27
+ */
+public interface IUserService extends IService<User> {
+
+}
